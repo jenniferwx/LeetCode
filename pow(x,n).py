e9e71b079110713ca6b8297714c5b2536fad8131
jenniferwx <wxjennifer@gmail.com>
2013-08-06 16:13:03 -0400

    insert main function

--- pow(x,n).py
+++ pow(x,n).py
@@ -1,4 +1,4 @@
-/*
+"""
 Author: Xi Wang
 Date: Aug 6, 2013
 Update: Aug 6, 2013
@@ -8,19 +8,22 @@ Source: http://leetcode.com/onlinejudge#question_50
 Notes:
 Implement pow(x, n).
 Solution: recursion.
-*/
+"""
 
 
 def pow(x,n):
   if x<0:
-     return (n%2==0)?pow(-x,n):-pow(-x,n)
-  elseif x==0:
+    if n%2==0:
+        return pow(-x,n)
+    else:
+        return -pow(-x,n)
+  elif x==0:
      return x
-  elseif n<0:
+  elif n<0:
      return 1.0/pow(-x,n)
-  elseif n==1:
+  elif n==1:
      return x
-  elseif n==0
+  elif n==0:
      return 1.0
   else:
      half = pow(x,n/2)
@@ -29,3 +32,7 @@ def pow(x,n):
      else:
        return half*half*x
 
+if __name__ =='__main__':
+    x = 3
+    n = 10
+    print pow(x,n) 
