349e170edb9a255453353416d6da90fd86052e6c
jenniferwx <wxjennifer@gmail.com>
2013-11-14 21:15:35 -0500

    Update SingleNumberII.cpp

--- SingleNumberII.cpp
+++ SingleNumberII.cpp
@@ -1,7 +1,11 @@
 /* Given an array of integers, every element appears three times except for one. Find that single one.
 
 Note:
-Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory? */
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory? 
+
+Written by Xi Wang
+11/14/2013
+*/
 
 class Solution {
 public:
