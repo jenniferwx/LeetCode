5eee46187184e62d1f4b1141c89ac0adcace68a0
jenniferwx <wxjennifer@gmail.com>
2013-11-23 22:19:12 -0500

    Update MaxPointsonaLine.cpp

--- MaxPointsonaLine.cpp
+++ MaxPointsonaLine.cpp
@@ -22,38 +22,38 @@ public:
         return size;
         int maxPoints = 0;
         for(int i = 0; i < size-1;i++)
+        {
+        	map<double,set<int>> mymap;
+		int duplicates = 0;
+		for(int j = ; j < size; j++)
 		{
-			map<double,set<int>> mymap;
-			int duplicates = 0;
-			for(int j = 0; j < size; j++)
-			{
-			  if(points[i].x==points[j].x && points[i].y == points[j].y && i!=j)
-				{
-				  duplicates++; 
-				  continue;
-				} // record the number of duplicate points
-				
-				double diffy = points[i].y-points[j].y;
-				double diffx = points[i].x-points[j].x;
-				double slope;
-				if(diffx==0)
-					slope = PI/2;
-				else if(diffy==0)
-					slope = 0;
-				else
-					slope = atan(diffy/diffx);
-				mymap[slope].insert(i);
-				mymap[slope].insert(j);
-			}
+		  if(points[i].x==points[j].x && points[i].y == points[j].y && i!=j)
+		{
+		  duplicates++; 
+		  continue;
+		} // record the number of duplicate points
+		
+		double diffy = points[i].y-points[j].y;
+		double diffx = points[i].x-points[j].x;
+		double slope;
+		if(diffx==0)
+	          slope = PI/2;
+		else if(diffy==0)
+		  slope = 0;
+		else
+		  slope = atan(diffy/diffx);
+		mymap[slope].insert(i);
+		mymap[slope].insert(j);
+		}
     	    map<double,set<int>>::iterator iter = mymap.begin();
             while(iter!=mymap.end())
             {
-    			      set<int> res = iter->second;
+    		set<int> res = iter->second;
                 if(res.size()+duplicates>maxPoints)
                    maxPoints = res.size()+duplicates;
                 iter++;
             }
-		}
+	}
 
         return maxPoints;
     }
