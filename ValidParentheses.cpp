6946800f4d7830f308a57d33ef3a50d6f6dad2f3
jenniferwx <wxjennifer@gmail.com>
2013-11-24 09:11:58 -0500

    Create ValidParentheses.cpp

---
+++ ValidParentheses.cpp
@@ -0,0 +1,47 @@
+/*
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input 
+string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+
+*/
+class Solution {
+public:
+    bool isValid(string s) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        stack<char> mystack;
+        int len = s.size();
+        if(len<=0)
+        return true;
+        for(int i=0;i<len;i++)
+        {
+            if(s[i]=='('||s[i]=='{'||s[i]=='[')
+            mystack.push(s[i]);
+            else
+            {
+                if(mystack.empty())
+                return false;
+                char top = mystack.top();
+                mystack.pop();
+                if(s[i]==')')
+                {
+                    if(top!='(')
+                    return false;
+                }
+                else if(s[i]==']')
+                {
+                    if(top!='[')
+                    return false;
+                }
+                else if(s[i]=='}')
+                {
+                    if(top!='{')
+                    return false;
+                }
+            }
+        }
+        return mystack.empty();
+        
+    }
+};
