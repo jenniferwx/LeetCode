d802c56f72bc154099e218fe63e98fe0187a0edc
jenniferwx <wxjennifer@gmail.com>
2013-11-26 21:51:08 -0500

    Add BFS solution

--- CloneGraph.cpp
+++ CloneGraph.cpp
@@ -40,6 +40,7 @@ public:
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
         return cloneGraph_1(node);
+        return cloneGraph_2(node);
     }
     // DFS
     Node *cloneGraph_1(Node *node) {
@@ -56,5 +57,33 @@ public:
         for (int i = 0; i < node->neighbors.size(); ++i)
             newNode->neighbors.push_back(cloneGraphRe(node->neighbors[i], map));
         return newNode;
+    }
+    
+        //BFS
+    Node *cloneGraph_2(Node *node) {
+        if (!node) return NULL;
+        queue<Node*> q;
+        q.push(node);
+        MAP map;
+        map[node] = new Node(node->label);
+        while (!q.empty())
+        {
+            Node *oriNode = q.front(); 
+            q.pop();
+            Node *newNode = map[oriNode];
+            for (int i = 0; i < oriNode->neighbors.size(); ++i)
+            {
+                Node *oriNeighbor = oriNode->neighbors[i];
+                if (map.find(oriNeighbor) != map.end()) {
+                    newNode->neighbors.push_back(map[oriNeighbor]);
+                    continue;
+                }
+                Node *newNeighbor = new Node(oriNeighbor->label);
+                newNode->neighbors.push_back(newNeighbor);
+                map[oriNeighbor] = newNeighbor;
+                q.push(oriNeighbor);
+            }
+        }
+        return map[node];
     }
 };
