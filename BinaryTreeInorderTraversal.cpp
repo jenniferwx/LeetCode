6b39fd8575e08d6979289e54deccc6dc5f2dda9e
jenniferwx <wxjennifer@gmail.com>
2014-01-01 17:58:02 -0500

    add iterative method

--- BinaryTreeInorderTraversal.cpp
+++ BinaryTreeInorderTraversal.cpp
@@ -1,6 +1,8 @@
 /*
 Given a binary tree, return the inorder traversal of its nodes' values.
 
+Note: Recursive solution is trivial, could you do it iteratively?
+
 */
 class Solution {
 public:
@@ -18,5 +20,26 @@ public:
         Inorder(root->left,result);
         result.push_back(root->val);
         Inorder(root->right,result);
+    }
+    // iterative method
+     vector<int> inorderTraversal_2(TreeNode *root) {
+        vector<int> res;
+        stack<TreeNode *> stk;
+        TreeNode *cur = root;
+        while (cur || !stk.empty())
+        {
+            if (cur)
+            {
+                stk.push(cur);
+                cur = cur->left;
+            }
+            else if (!stk.empty())
+            {
+                res.push_back(stk.top()->val);
+                cur = stk.top()->right;
+                stk.pop();
+            }
+        }
+        return res;
     }
 };
